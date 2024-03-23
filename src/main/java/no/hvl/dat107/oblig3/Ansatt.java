dcde2b45f3ba23c1268571733b00fb23f5b1430c
Preben <587726@stud.hvl.no>
2024-03-23 13:25:44 +0100

    Ansatt entity added.

---
+++ src/main/java/no/hvl/dat107/oblig3/Ansatt.java
@@ -0,0 +1,110 @@
+package no.hvl.dat107.oblig3;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity
+@Table(schema = "firmadb", name = "ansatt")
+public class Ansatt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    //Unqiue
+    private String brukernavn;
+
+    @Column
+    private String fornavn;
+
+    @Column
+    private String etternavn;
+
+    @Column
+    private Date ansettelse;
+
+    @Column
+    private String stilling;
+
+    @Column
+    private Integer manedslonn;
+
+    @Column
+    private Integer avdeling;
+
+    public Ansatt(){
+
+    }
+
+    public String getBrukernavn() {
+        return brukernavn;
+    }
+
+    public void setBrukernavn(String brukernavn) {
+        this.brukernavn = brukernavn;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public Date getAnsettelse() {
+        return ansettelse;
+    }
+
+    public void setAnsettelse(Date ansettelse) {
+        this.ansettelse = ansettelse;
+    }
+
+    public String getStilling() {
+        return stilling;
+    }
+
+    public void setStilling(String stilling) {
+        this.stilling = stilling;
+    }
+
+    public Integer getManedslonn() {
+        return manedslonn;
+    }
+
+    public void setManedslonn(Integer manedslonn) {
+        this.manedslonn = manedslonn;
+    }
+
+    public Integer getAvdeling() {
+        return avdeling;
+    }
+
+    public void setAvdeling(Integer avdeling) {
+        this.avdeling = avdeling;
+    }
+
+    @Override
+    public String toString() {
+        return "Ansatt{" +
+                "id=" + id +
+                ", brukernavn='" + brukernavn + '\'' +
+                ", fornavn='" + fornavn + '\'' +
+                ", etternavn='" + etternavn + '\'' +
+                ", ansettelse=" + ansettelse +
+                ", stilling='" + stilling + '\'' +
+                ", manedslonn=" + manedslonn +
+                ", avdeling=" + avdeling +
+                '}';
+    }
+}
